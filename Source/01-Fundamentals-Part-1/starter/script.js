f31919fbcaab777fde18dc18d320d60b01c4d6e4
retelesk <tranhongquan.jb@gmail.com>
2022-10-13 11:50:22 +0700

    modified:   Source/01-Fundamentals-Part-1/starter/index.html 	new file:   Source/01-Fundamentals-Part-1/starter/script.js

---
+++ Source/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,23 @@
+let js = "amazing";
+
+console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Maltida";
+let first = "Jonas";
+let firstNamePerson;
+let first_name;
+let first_name_person;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let person = "Jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
