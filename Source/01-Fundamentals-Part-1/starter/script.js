ec5b80abcfd52d02bba32e87b6f380a505633b7e
retelesk <tranhongquan.jb@gmail.com>
2022-10-13 14:25:39 +0700

    modified:   Source/01-Fundamentals-Part-1/starter/script.js

--- Source/01-Fundamentals-Part-1/starter/script.js
+++ Source/01-Fundamentals-Part-1/starter/script.js
@@ -23,6 +23,73 @@ let myFirstJob = "Programmer";
 let myCurrentJob = "Teacher";
 
 console.log(myFirstJob);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "ATLAS");
+
+console.log("----------------");
+javascriptIsFun = "ATLAS";
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 10;
+
+// const job;
+
+var job = "programer";
+job = "teacher";
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 *2
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); //>, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log((now = 1991 > now - 2018));
 */
-true;
-console.log(true);
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log((now = 1991 > now - 2018));
+console.log(25 - 10 - 5);
