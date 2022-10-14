e4af19ae9d3daed013e99225663288017b6b2516
retelesk <tranhongquan.jb@gmail.com>
2022-10-15 01:15:06 +0700

    modified:   Source/02-Fundamentals-Part-2/starter/script.js

---
+++ Source/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,68 @@
+"use strict";
+
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) {
+  hasDriversLicense = true;
+}
+if (hasDriversLicense) {
+  console.log(`I can drive :p`);
+}
+
+const interface = `Audio`;
+const private = 423;
+
+function logger() {
+  console.log(`My name is Jonas`);
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(2, 4));
+
+const appleOrangeJuice = fruitProcessor(5, 4);
+console.log(appleOrangeJuice);
+
+// Function declaration
+const age1 = calcAge1(1991);
+
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+console.log(age1);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+*/
+// Arrow function
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
+console.log(yearsUntilRetirement(1980, "Bob"));
