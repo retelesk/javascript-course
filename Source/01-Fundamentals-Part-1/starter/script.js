d6553559506cc811327c6f00b67afd286e327e28
retelesk <tranhongquan.jb@gmail.com>
2022-10-14 09:54:53 +0700

    new file:   .fleet/settings.json 	modified:   Source/01-Fundamentals-Part-1/starter/script.js

--- Source/01-Fundamentals-Part-1/starter/script.js
+++ Source/01-Fundamentals-Part-1/starter/script.js
@@ -136,7 +136,17 @@ lines new`);
 const age = 15;
 
 if (age >= 18) {
-    console.log(`Sarah can start driving license`);
+  console.log(`Sarah can start driving license`);
 } else {
-    console.log(`Sarah can't start driving license`);
+  const yearLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearLeft} years :)`);
 }
+
+const birthYear = 2012;
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
