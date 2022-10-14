08de052668681d2606573dc5d0785a41e191d501
retelesk <tranhongquan.jb@gmail.com>
2022-10-14 19:50:13 +0700

    modified:   Source/01-Fundamentals-Part-1/starter/script.js

--- Source/01-Fundamentals-Part-1/starter/script.js
+++ Source/01-Fundamentals-Part-1/starter/script.js
@@ -297,6 +297,34 @@ if (averageScoreDolphins > averageScoreKoalas && averageScoreDolphins > 100) {
 } else {
   console.log(`No teams won`);
 }
+
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas) {
+  console.log(`Dolphins win the trophy 🏆`);
+} else if (scoreDolphins < scoreKoalas) {
+  console.log(`Koalas win the trophy 🏆`);
+} else if (scoreDolphins === scoreKoalas) {
+  console.log(`Both win the trophy! `);
+}
 */
-const scoreDolphins = (96 + 112 + 101) / 3;
-const scoreKoalas = (109 + 95 + 106) / 3;
+// BONUS 1
+const scoreDolphins = (97 + 112 + 101) / 3;
+const scoreKoalas = (109 + 95 + 123) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log(`Dolphins win the trophy 🏆`);
+} else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
+  console.log(`Koalas win the trophy 🏆`);
+} else if (
+  scoreDolphins === scoreKoalas &&
+  scoreDolphins >= 100 &&
+  scoreKoalas >= 100
+) {
+  console.log(`Both win the trophy! `);
+} else {
+  console.log(`No team wins`);
+}
