ce73e6f01deb09363850c9e13f353cd54aabfa10
retelesk <tranhongquan.jb@gmail.com>
2022-10-14 12:30:38 +0700

    modified:   script.js

--- Source/01-Fundamentals-Part-1/starter/script.js
+++ Source/01-Fundamentals-Part-1/starter/script.js
@@ -169,9 +169,16 @@ if (BMIMark > BMIJohn) {
   console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})`);
 }
 */
+// Type conversion
+
 const inputYear = '1991';
 console.log(Number(inputYear), inputYear);
 console.log(inputYear + 18, Number(inputYear) + 18);
 
 console.log(Number("Jonas"));
 console.log(typeof NaN);
+
+console.log(String(23),23);
+
+// Type coercion
+console.log('I am '+23+' years old');
