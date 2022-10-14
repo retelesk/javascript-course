f9870d59403a444804129a1b221c298a6a491eaa
retelesk <tranhongquan.jb@gmail.com>
2022-10-14 11:25:01 +0700

    modified:   Source/01-Fundamentals-Part-1/starter/script.js

--- Source/01-Fundamentals-Part-1/starter/script.js
+++ Source/01-Fundamentals-Part-1/starter/script.js
@@ -132,7 +132,7 @@ lines');
 console.log(`String
 multiple
 lines new`);
-*/
+
 const age = 15;
 
 if (age >= 18) {
@@ -150,3 +150,21 @@ if (birthYear <= 2000) {
   century = 21;
 }
 console.log(century);
+*/
+// CODING CHALLENGE 2
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+if (BMIMark > BMIJohn) {
+  console.log(
+    `Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})
+    })`
+  );
+} else {
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})`);
+}
