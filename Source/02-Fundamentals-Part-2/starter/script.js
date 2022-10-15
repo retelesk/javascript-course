685534a37196bb06d9c41b61adb141c696ac5de2
retelesk <tranhongquan.jb@gmail.com>
2022-10-15 20:47:07 +0700

    modified:   script.js

--- Source/02-Fundamentals-Part-2/starter/script.js
+++ Source/02-Fundamentals-Part-2/starter/script.js
@@ -51,7 +51,7 @@ const calcAge2 = function (birthYear) {
 
 const age2 = calcAge2(1991);
 console.log(age1, age2);
-*/
+
 // Arrow function
 const calcAge3 = (birthYear) => 2037 - birthYear;
 const age3 = calcAge3(1991);
@@ -66,3 +66,28 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(1991, "Jonas"));
 console.log(yearsUntilRetirement(1980, "Bob"));
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} apple and ${orangePieces} orange.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3))
+*/
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+};
+console.log(yearsUntilRetirement(1991, 'Jonas'));
