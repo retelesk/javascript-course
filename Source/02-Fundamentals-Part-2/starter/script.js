a0208657486ec886375f49a1a1dd49e7bce151ab
retelesk <tranhongquan.jb@gmail.com>
2022-10-19 16:08:37 +0700

    modified:   Source/02-Fundamentals-Part-2/starter/script.js

--- Source/02-Fundamentals-Part-2/starter/script.js
+++ Source/02-Fundamentals-Part-2/starter/script.js
@@ -215,7 +215,7 @@ console.log(`${bills}
 ${tips}
 ${totals}`);
 
-*/
+
 const jonas = {
   fistName: "Jonas",
   lastName: "Schmedtmann",
@@ -224,3 +224,132 @@ const jonas = {
   friends: ["Michael", "Peter", "Steven"],
 };
 console.log(jonas);
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
+console.log(jonas);
+console.log(`
+`);
+
+console.log(jonas.lastName);
+console.log(jonas["lastName"]);
+
+const nameKey = "Name";
+console.log(jonas["first" + nameKey]);
+console.log(jonas["last" + nameKey]);
+
+// console.log(jonas.'last'+nameKey) | Not Work
+
+// const interestedIn = prompt(
+//   "What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends"
+// );
+// if (jonas[interestedIn]) {
+//   console.log(jonas[interestedIn]);
+// } else {
+//   console.log(
+//     `Wrong request! Choose between firstName, lastName, age, job and friends`
+//   );
+// }
+
+jonas.location = "Portugal";
+jonas["twitter"] = "@jonasschmedtmann";
+console.log(jonas);
+console.log(
+  `${jonas["firstName"]} has ${jonas["friends"].length} friends, and his best friend is called ${jonas["friends"][0]}`
+);
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
+);
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  birthYear: 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  hasDriversLicense: true,
+
+  // calcAge: function (birthYear) {
+  //   return 2037 - birthYear;
+  // },
+  // calcAge: function () {
+  //   // console.log(this);
+  //   return 2037 - this.birthYear;
+  // },
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${
+      this.job
+    }, and he ${this.hasDriversLicense ? "a" : "no"} driver's license`;
+  },
+};
+console.log(jonas.calcAge());
+console.log(jonas.age);
+console.log(jonas.getSummary());
+
+// Challenge
+// "Jonas is a 46-year old teacher, and he has a driver's license"
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+mark.bmi > john.bmi
+  ? console.log(
+      `${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than ${
+        john.fullName
+      }'s (${john.calcBMI()})`
+    )
+  : console.log(
+      `${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${
+        mark.fullName
+      }'s (${mark.calcBMI()})`
+    );
+
+// For loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weight repetition ${rep}`);
+}
+
+const jonasArray = [
+  "jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+  true,
+];
+const jonasType = [];
+for (let i = 0; i < jonasArray.length; i++) {
+  // Reading from Jonas Array
+  console.log(jonasArray[i], typeof jonasArray[i]);
+  // Filling types array
+  // jonasType[i] = typeof jonasArray[i];
+  jonasType.push(typeof jonasArray[i]);
+}
+console.log(jonasType);
+*/
+const years = [1991, 2007, 1969, 2020];
