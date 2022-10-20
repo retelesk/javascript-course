acfa1f47be6ef93877b029b2670c9552000b7b61
retelesk <tranhongquan.jb@gmail.com>
2022-10-20 23:04:16 +0700

    modified:   Source/05-Guess-My-Number/starter/script.js

--- Source/05-Guess-My-Number/starter/script.js
+++ Source/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number! 🎉';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 15;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
