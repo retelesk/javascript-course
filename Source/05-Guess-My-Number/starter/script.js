ca8056d366d7ec77efd069d38aba11d832f0c3aa
retelesk <tranhongquan.jb@gmail.com>
2022-10-22 18:48:34 +0700

    modified:   Source/05-Guess-My-Number/starter/script.js

--- Source/05-Guess-My-Number/starter/script.js
+++ Source/05-Guess-My-Number/starter/script.js
@@ -10,14 +10,11 @@ document.querySelector('.score').textContent = 15;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
 
   // When there is no input
   if (!guess) {
@@ -29,6 +26,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.highscore').textContent = score;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
 
     // When guess is too high
   } else if (guess > secretNumber) {
@@ -51,4 +49,14 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
